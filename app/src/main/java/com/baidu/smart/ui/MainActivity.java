6856171fabad911aae7b0ec3b0163aa7f1a0d30e
lishuai <lishaui@dongqiudi.com>
2016-01-31 12:50:24 +0800

    添加xlistview，在mainactivity中有simple

--- app/src/main/java/com/baidu/smart/ui/MainActivity.java
+++ app/src/main/java/com/baidu/smart/ui/MainActivity.java
@@ -4,15 +4,32 @@ package com.baidu.smart.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ArrayAdapter;
 
 import com.baidu.smart.R;
+import com.baidu.third.xlistview.XListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by lishuai on 16/1/29.
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements XListView.IXListViewListener {
 
     public static final String TAG = "MainActivity";
+
+    private XListView mListView;
+
+    private ArrayAdapter<String> mAdapter;
+
+    private ArrayList<String> items = new ArrayList<String>();
+
+    private Handler mHandler;
+
+    private int start = 0;
+
+    private static int refreshCnt = 0;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
@@ -20,8 +37,57 @@ public class MainActivity extends BaseActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        geneItems();
+        mListView = (XListView)findViewById(R.id.xListView);
+        mListView.setPullLoadEnable(true);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list_item, items);
+        mListView.setAdapter(mAdapter);
+        // mListView.setPullLoadEnable(false);
+        // mListView.setPullRefreshEnable(false);
+        mListView.setXListViewListener(this);
+        mHandler = new Handler();
+    }
+
+    private void geneItems() {
+        for (int i = 0; i != 20; ++i) {
+            items.add("refresh cnt " + (++start));
+        }
+    }
+
+    private void onLoad() {
+        mListView.stopRefresh();
+        mListView.stopLoadMore();
+        mListView.setRefreshTime("刚刚");
+    }
+
+    @Override
+    public void onRefresh() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                start = ++refreshCnt;
+                items.clear();
+                geneItems();
+                // mAdapter.notifyDataSetChanged();
+                mAdapter = new ArrayAdapter<>(MainActivity.this, R.layout.list_item, items);
+                mListView.setAdapter(mAdapter);
+                onLoad();
+            }
+        }, 2000);
+    }
+
+    @Override
+    public void onLoadMore() {
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                geneItems();
+                mAdapter.notifyDataSetChanged();
+                onLoad();
+            }
+        }, 2000);
     }
 }
