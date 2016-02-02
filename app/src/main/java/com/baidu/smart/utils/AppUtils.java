fd7bc6324f36fa5fe53960fa70cdc318e68f1f3e
lishuai <lishaui@dongqiudi.com>
2016-02-02 18:29:49 +0800

    添加fresco，具体查看wiki

--- app/src/main/java/com/baidu/smart/utils/AppUtils.java
+++ app/src/main/java/com/baidu/smart/utils/AppUtils.java
@@ -1,47 +1,26 @@
 
 package com.baidu.smart.utils;
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Build;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
-import com.baidu.smart.R;
-import com.baidu.smart.third.SystemBarTintManager;
+import android.net.Uri;
 
 /**
  * Created by lishuai on 16/1/29.
  */
 public class AppUtils {
 
-    public static void setImmersionType(Activity activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = activity.getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView()
-                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-            SystemBarTintManager tintManager = new SystemBarTintManager(activity);
-            tintManager.setStatusBarTintEnabled(true);
-            tintManager.setStatusBarTintResource(R.color.theme);
-        } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.KITKAT) {
-            Window win = activity.getWindow();
-            WindowManager.LayoutParams winParams = win.getAttributes();
-            final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
-            winParams.flags |= bits;
-            win.setAttributes(winParams);
-            SystemBarTintManager mTintManager = new SystemBarTintManager(activity);
-            mTintManager.setStatusBarTintEnabled(true);
-            mTintManager.setNavigationBarTintEnabled(true);
-            mTintManager.setTintColor(activity.getResources().getColor(R.color.theme));
-            // mTintManager.setTintDrawable(UIElementsHelper
-            // .getGeneralActionBarBackground(activity));
+    /**
+     * fresco 设置图片非空判断
+     * 
+     * @param urlStr
+     * @return
+     */
+    public static Uri parse(String urlStr) {
+        Uri uri = null;
+        if (urlStr == null) {
+            uri = Uri.parse("");
+        } else {
+            uri = Uri.parse(urlStr);
         }
+        return uri;
     }
 }
