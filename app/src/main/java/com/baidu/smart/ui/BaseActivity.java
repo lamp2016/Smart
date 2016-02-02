cb4dbace9daca2e56396206c50493fc4fd37066d
lishuai <lishaui@dongqiudi.com>
2016-02-02 17:00:46 +0800

    添加沉浸式主题

--- app/src/main/java/com/baidu/smart/ui/BaseActivity.java
+++ app/src/main/java/com/baidu/smart/ui/BaseActivity.java
@@ -1,25 +1,50 @@
 
 package com.baidu.smart.ui;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.baidu.smart.R;
+import com.baidu.smart.utils.SystemBarTintManager;
 
 /**
  * Created by lishuai on 16/1/29.
  */
 public class BaseActivity extends Activity {
-    // public WindowManager mWindowManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // x.view().inject(this);
-        // 沉浸式状态栏
-        // mWindowManager =
-        // (WindowManager)getSystemService(Context.WINDOW_SERVICE);
-        // requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // AppUtils.setImmersionType(this);
 
+        init();
+    }
+
+    private void init() {
+        // 沉浸式状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus(true);
+        }
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintEnabled(true);
+        tintManager.setStatusBarTintResource(R.color.theme);//通知栏所需颜色
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus(boolean on) {
+        Window win = getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
 }
